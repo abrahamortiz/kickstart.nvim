fb93d13aebff068ab5d223482beff713bc087d9a
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-07-15 15:10:42 +0200

    style: set transparent true

--- lua/custom/plugins/kanagawa.lua
+++ lua/custom/plugins/kanagawa.lua
@@ -2,6 +2,8 @@ vim.pack.add {
   { src = 'https://github.com/rebelot/kanagawa.nvim' },
 }
 
-require('kanagawa').setup {}
+require('kanagawa').setup {
+  transparent = true,
+}
 
 vim.cmd.colorscheme 'kanagawa-wave'
