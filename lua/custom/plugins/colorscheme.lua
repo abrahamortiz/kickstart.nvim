5c59de5bc7d5765ce6bbc2ce03acd8b153d38557
Abraham Ortiz <abraham.ortiz@leovegas.com>
2026-07-24 13:38:04 +0200

    chore: change to ayu colorscheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,20 +1,21 @@
 vim.pack.add {
-  { src = 'https://github.com/maxmx03/solarized.nvim' },
+  'https://github.com/shatur/neovim-ayu',
 }
 
-require('solarized').setup {
-  transparent = {
-    enabled = true, -- Master switch to enable transparency
-    pmenu = true, -- Popup menu (e.g., autocomplete suggestions)
-    normal = true, -- Main editor window background
-    normalfloat = true, -- Floating windows
-    neotree = true, -- Neo-tree file explorer
-    nvimtree = true, -- Nvim-tree file explorer
-    whichkey = true, -- Which-key popup
-    telescope = true, -- Telescope fuzzy finder
-    lazy = true, -- Lazy plugin manager UI
-    mason = true, -- Mason manage external tooling
-  },
+require('ayu').setup {
+  overrides = function()
+    return {
+      Normal = { bg = 'None' },
+      NormalFloat = { bg = 'None' },
+      ColorColumn = { bg = 'None' },
+      SignColumn = { bg = 'None' },
+      Folded = { bg = 'None' },
+      FoldColumn = { bg = 'None' },
+      CursorLine = { bg = 'None' },
+      CursorColumn = { bg = 'None' },
+      VertSplit = { bg = 'None' },
+    }
+  end,
 }
 
-vim.cmd.colorscheme 'solarized'
+vim.cmd.colorscheme 'ayu-mirage'
